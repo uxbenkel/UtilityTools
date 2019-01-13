31f28e4d6fca11a4d90ac7ed73d5995ffaacdfeb
Jerry He <JerryB0521@gmail.com>
2019-01-13 16:58:09 +0800

    First Commit

---
+++ src/main/java/com/hetao/utils/TestDemo.java
@@ -0,0 +1,12 @@
+package com.hetao.utils;
+
+/**
+ * @author hetao
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println(3 | 2);
+        System.out.println(3 & 2);
+        System.out.println(3 ^ 2);
+    }
+}
