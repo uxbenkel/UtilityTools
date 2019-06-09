740559e5f5a4a1aa4db415da546813479e151692
hetao <JerryB0521@gmail.com>
2019-06-09 17:24:21 +0800

    commit

--- src/main/java/com/hetao/utils/TestDemo.java
+++ src/main/java/com/hetao/utils/TestDemo.java
@@ -1,16 +1,10 @@
 package com.hetao.utils;
 
-import org.apache.poi.POIXMLDocument;
-import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-
 /**
  * @author hetao
  */
 public class TestDemo {
-    public static void main(String[] args) throws Exception {
-        OPCPackage opcPackage = POIXMLDocument.openPackage("/Users/hetao/Downloads/Inbox/correspond8.docx");
-        String text1 = new XWPFWordExtractor(opcPackage).getText();
-        System.out.println(text1);
+    public static void main(String[] args) {
+        System.out.println("123");
     }
 }
